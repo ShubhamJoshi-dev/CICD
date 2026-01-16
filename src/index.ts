cc8faa509d4dd2e02f93038873cadc86d104021f
ShubhamJoshi-dev <codefortanek@gmail.com>
2026-01-16 18:58:39 +0545

    add random endpoint

--- src/index.ts
+++ src/index.ts
@@ -18,6 +18,28 @@ app.get("/health", (req: Request, res: Response) => {
   return res.status(200).json(payload);
 });
 
+app.get("/random", (req: Request, res: Response) => {
+  const messages = [
+    "Working",
+    "Service is healthy",
+    "Random response generated",
+    "Everything looks good",
+    "Mock API response",
+  ];
+
+  const statuses = ["SUCCESS", "OK", "ACTIVE"];
+  const randomItem = (arr: any) => arr[Math.floor(Math.random() * arr.length)];
+
+  const payload = {
+    message: randomItem(messages),
+    status: randomItem(statuses),
+    requestId: Math.random().toString(36).substring(2, 10),
+    time: new Date().toISOString(),
+  };
+
+  return res.status(201).json(payload);
+});
+
 app.listen(port, () => {
   console.log(`Server is Running on the Port : ${port}`);
 });
